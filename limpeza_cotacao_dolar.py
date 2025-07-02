1986a574b2479e1f064bbf0c799bb6f284e63f1b
Samurai <davi.cruz1@estudante.ifb.edu.br>
2025-07-02 17:53:33 -0300

    feat - Transformação dos dados

--- limpeza_cotacao_dolar.py
+++ limpeza_cotacao_dolar.py
@@ -16,4 +16,41 @@ except FileNotFoundError:
     print("Execute o script 'ingerir_cotacao_dolar.py' primeiro")
     exit() #encerra o script se o arquivo não existir
 
-    
+#Olhar os dados depois de carregar
+print("\n--- Primeiras 5 linhas dos dados brutos ---")
+print(df.head())
+
+print("\n--- Informações sobre os tipos de dados ---")
+df.info()
+
+print("\nIniciando a limpeza e transformação...")
+
+#Converte a coluna para o tipo datetime
+df['dataHoraCotacao'] = pd.to_datetime(df['dataHoraCotacao'])
+
+print("\nColuna 'dataHoraCotacao' convertida para o tipo datetime.")
+
+# Formata a data como um texto no padrão que quisermos.
+df['data_criacao'] = df['dataHoraCotacao'].dt.strftime('%Y-%m-%d')
+
+df['hora_criacao'] = df['dataHoraCotacao'].dt.strftime('%H:%M')
+
+print("Colunas 'data_criacao' e 'hora_criacao' criadas.")
+
+print("\n--- Primeiras 5 linhas dos dados transformados ---")
+# Mostramos as colunas relevantes para ver a transformação
+print(df[['dataHoraCotacao', 'data_criacao', 'hora_criacao']].head())
+
+# Define o nome da pasta de destino
+nome_pasta_staging = "staging"
+
+# Cria a pasta "staging" se ela não existir
+os.makedirs(nome_pasta_staging, exist_ok=True)
+
+# Define o caminho do novo arquivo
+caminho_arquivo_staging = os.path.join(nome_pasta_staging, "cotacao_dolar_tratada.csv")
+
+# Salva o DataFrame transformado em um novo arquivo .csv
+df.to_csv(caminho_arquivo_staging, index=False, sep=';', decimal=',')
+
+print(f"\nSucesso! Os dados limpos foram salvos em: {caminho_arquivo_staging}")
